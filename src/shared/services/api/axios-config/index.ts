2cd8fc4e60aba620e520dc815f90a4215a4c99da
Viniciuss2207 <vini22reinteria@gmail.com>
2022-06-26 15:03:46 -0300

    feat: Axios

---
+++ src/shared/services/api/axios-config/index.ts
@@ -0,0 +1,15 @@
+import axios from 'axios'
+import { errorInterceptor, responseInterceptor } from './interceptors';
+
+
+const Api = axios.create({
+    baseURL: 'http://localhost:3333'
+});
+
+
+Api.interceptors.response.use(
+    (response) => responseInterceptor(response), 
+    (response) => errorInterceptor(response), 
+);
+
+export { Api};
