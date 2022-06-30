95690343aa4fb6852bf6ee998bb497193779edc3
Viniciuss2207 <vini22reinteria@gmail.com>
2022-06-30 13:59:25 -0300

    ft:impedir consultas uniteis com o useDebounce

--- src/pages/pessoas/ListagemDePessoas.tsx
+++ src/pages/pessoas/ListagemDePessoas.tsx
@@ -1,27 +1,33 @@
 import { useEffect, useMemo } from "react";
 import { useSearchParams } from "react-router-dom"
+
 import { FerramentasDalistagem } from "../../shared/components"
+import { useDebounce } from "../../shared/hooks";
 import { LayoutBaseDePagina } from "../../shared/layouts"
 import { PessoasService } from "../../shared/services/api/pessoas/PessoasService";
 
 export const ListagemDePessoas: React.FC = () => {
     const [searchParams, setSearchParams] = useSearchParams();
 
+    const { debounce } = useDebounce(3000, false);
+    
     const busca = useMemo(() => {
         return searchParams.get('busca') || '';
     }, [searchParams]  )
 
-    useEffect(() =>{
-        
-        PessoasService.getAll(1, busca)
-        .then((result) =>{
-            if(result instanceof Error){
-                alert(result.message)
-            } else{
-             console.log(result);   
-            }      
+    useEffect(() => {
+
+        debounce(() => {
+          PessoasService.getAll(1, busca)
+            .then((result) => {
+              if (result instanceof Error) {
+                alert(result.message);
+              } else {
+                console.log(result);
+              }
+            });
         });
-    },[busca]);
+      }, [busca]);
 
     return(
         <LayoutBaseDePagina 
