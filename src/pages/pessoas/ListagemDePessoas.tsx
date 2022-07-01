640d62a478d73bca6d33ec6148b50226ca88ed3f
Viniciuss2207 <vini22reinteria@gmail.com>
2022-07-01 12:01:55 -0300

    ft:Adicionando loading na listagem de pessoas

--- src/pages/pessoas/ListagemDePessoas.tsx
+++ src/pages/pessoas/ListagemDePessoas.tsx
@@ -1,4 +1,4 @@
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import { LinearProgress, Paper, Table, TableBody, TableCell, TableContainer, TableFooter, TableHead, TableRow } from "@mui/material";
 import { useEffect, useMemo, useState } from "react";
 import { useSearchParams } from "react-router-dom"
 
@@ -72,7 +72,17 @@ export const ListagemDePessoas: React.FC = () => {
              </TableRow>
              ))}
             </TableBody>
-
+              <TableFooter>
+                {isLoading && (
+              <TableRow>
+                <TableCell  colSpan={3} >
+                   
+                  <LinearProgress variant='indeterminate' />
+                
+                </TableCell>
+              </TableRow>
+              )}
+              </TableFooter>
             </Table>
           </TableContainer>
         </LayoutBaseDePagina>
