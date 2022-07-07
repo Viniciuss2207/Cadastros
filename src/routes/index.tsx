5294a3252e5dfcaefc5840b74bc6afca17008087
Viniciuss2207 <vini22reinteria@gmail.com>
2022-07-07 14:40:50 -0300

    add: Tela de criação e edição de registro

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,7 @@
 
 import { useEffect } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import { Dashboard,ListagemDePessoas } from '../pages';
+import { Dashboard,ListagemDePessoas,DetalheDePessoas } from '../pages';
 import { useDrawerContext } from '../shared/contexts';
 export const AppRoutes = () => {
   const {  setDrawerOptions } = useDrawerContext();
@@ -27,7 +27,7 @@ export const AppRoutes = () => {
       <Route path="/pagina-inicial" element={<Dashboard/>} />
       <Route path="/pessoas" element={<ListagemDePessoas/>} />
 
-      <Route path="/pessoas/detalhe/:id" element={<p>Detalhes</p>} /> 
+      <Route path="/pessoas/detalhe/:id" element={<DetalheDePessoas/>} /> 
 
      <Route path="*" element={<Navigate to="/pagina-inicial"/>}/>
     </Routes>
